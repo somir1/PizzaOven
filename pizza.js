d24df8a1958f9106f7ca592dec401b4cd8acf8ed
somir1 <leonsomir@gmail.com>
2021-06-28 16:32:26 -0500

    Bonus Challenge

--- pizza.js
+++ pizza.js
@@ -18,6 +18,55 @@ console.log(p3);
 var p4 = pizzaOven("Hand tossed", "Traditional", ["Gargonzola"], ["Chicken", "Banana Peppers", "Pineapple"]);
 console.log(p4);
 
-function randomPizza(){
 
+var CrustType = [
+    "Hand tossed",
+    "Thin and Crispy",
+    "Deep Dish"
+];
+
+var SauceType = [
+    "Tradional",
+    "Buffalo",
+    "Marinara",
+    "Alfredo"
+];
+
+var Cheeses = [
+    "Mozzerrella",
+    "Gargonzola",
+    "Feta",
+    "Chedder"
+];
+
+var Toppings = [
+    "Pepporini",
+    "Chicken",
+    "Olives",
+    "Ham",
+    "Pineapple"
+];
+
+function randomRange(max, min){
+    return Math.floor(Math.random() * max - min) + min;
 }
+
+function randomSelect(arr){
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+function randomPizza(){
+    var pizza = {}
+    pizza.CrustType = randomSelect(CrustType);
+    pizza.SauceType = randomSelect(SauceType);
+    pizza.Cheeses = [];
+    pizza.Toppings =  [];
+    for(var i = 0; i < randomRange(3,1); i++){
+        pizza.Cheeses.push(randomSelect(Cheeses));
+    }
+    for(var i = 0; i < randomRange(4,1); i++){
+        pizza.Toppings.push(randomSelect(Toppings));
+    }
+    return pizza;
+}
+console.log(randomPizza())
